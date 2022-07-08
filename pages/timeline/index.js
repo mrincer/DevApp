4978cfc3c264bd37c0dd778ba9ec13818b47707c
Kirk Incer <kirk.incer@ahinko.com>
2022-07-07 20:36:55 -0600

    DevApp base created

---
+++ pages/timeline/index.js
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function Timeline() {
+    return (
+        <>
+            <h1>Timeline</h1>
+            <Link href="/"><a>Home</a></Link>
+        </>
+    )
+}
